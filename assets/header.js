4c2f05ad7da489418fb0fd7f4602e44c40366326
sparkyJSlibary <ianv6207@mytusd.org>
2026-03-13 08:51:01 -0700

    Fix typo in header image height assignment

--- assets/header.js
+++ assets/header.js
@@ -4,7 +4,7 @@ const headerHomeS = document.createElement("a");
 headerHomeS.href = "https://sparkyJSlibary.github.io/";
 const headerHomeI = document.createElement("img");
 headerHomeI.width = "200";
-headrerHomeI.height = "45";
+headerHomeI.height = "45";
 headerHomeI.src = "https://sparkyJSlibary.github.io/assets/images/icons/headerImg.png";
 document.body.appendChild(header);
 header.appendChild(headerHomeS);
