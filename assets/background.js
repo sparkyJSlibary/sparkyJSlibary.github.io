3ef233260228a7a198d2311e4ffe09d0327e6182
sparkyJSlibary <ianv6207@mytusd.org>
2026-03-13 11:47:54 -0300

    Update background.js

--- assets/background.js
+++ assets/background.js
@@ -1,6 +1,6 @@
 const bg = document.createElement("img");
-  bg.src = "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-1257089-2387793.jpg&fm=jpg";
-  bg.style.width = "100%";
-  bg.style.height = "100%";
-  bg.style.zIndex = -9999;
+bg.src = "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-1257089-2387793.jpg&fm=jpg";
+bg.style.width = "100%";
+bg.style.height = "100%";
+bg.style.zIndex = -9999;
 document.body.appendChild(bg);
