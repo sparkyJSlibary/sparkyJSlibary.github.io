310a047ab70a5266155c14890c41abe6399a78b9
sparkyJSlibary <ianv6207@mytusd.org>
2026-03-13 12:09:11 -0700

    Update background.js

--- assets/background.js
+++ assets/background.js
@@ -1,5 +1,5 @@
 const bg = document.createElement("img");
-bg.src = "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-1257089-2387793.jpg&fm=jpg";
+bg.src = "https://images.unsplash.com/photo-1674421268449-d68facc81eca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 bg.style.width = "100%";
 bg.style.height = "100%";
 bg.style.zIndex = -9999;
