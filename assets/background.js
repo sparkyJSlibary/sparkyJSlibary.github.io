76814d163061e4fe56e614b7423ae898a24f09bb
sparkyJSlibary <ianv6207@mytusd.org>
2026-03-13 00:40:06 -0300

    Create background.js

---
+++ assets/background.js
@@ -0,0 +1,6 @@
+const background = document.createElement("img");
+  img.src = "https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg?cs=srgb&dl=pexels-adrien-olichon-1257089-2387793.jpg&fm=jpg";
+  img.style.width = "100%";
+  img.style.height = "100%";
+  img.style.zIndex = -9999;
+document.body.appendChild(background);
