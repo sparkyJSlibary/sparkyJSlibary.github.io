703f5ac0ef4c698456baf399c9798049e582c9e6
sparkyJSlibary <ianv6207@mytusd.org>
2026-03-13 23:56:55 -0300

    Add examplecandp.js with animal creation example
    Add example JavaScript code for creating a 'pug' animal.

---
+++ assets/examplecandp.js
@@ -0,0 +1,6 @@
+let examples = {
+  "example1" : `
+  <p><span style="color: red">const</span><span style="color: orange"> pug</span> = <span>sparky.new("animal", "https://i.pinimg.com/originals/15/43/98/154398cd9fbcf9eca96273abf6911c1d.gif", "50", "50", "0px", "0px", null);
+  `
+}
+const pug = sparky.new("animal", "https://i.pinimg.com/originals/15/43/98/154398cd9fbcf9eca96273abf6911c1d.gif", "50", "50", "0px", "0px", null);
